9fed664306652c923d5d7e7e348d91ffa00a7a35
leotada <leotada523@gmail.com>
2019-12-19 20:06:13 -0300

    More installation information

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ async def crawler(request, url: str) -> HTTPResponse:
     return json({'message': 'Timeout reached!'})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, access_log=True)
